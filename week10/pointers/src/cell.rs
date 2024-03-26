dca1aa0bd17f2d07a71d5300acabf3a37d83e665
Connor Tsui <connor.tsui20@gmail.com>
2024-03-26 18:05:34 -0400

    first commit

---
+++ week10/pointers/src/cell.rs
@@ -0,0 +1,20 @@
+pub struct Cell<T> {
+    value: T,
+}
+
+impl<T> Cell<T> {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn set(&self, value: T) {
+        todo!()
+    }
+
+    pub fn get(&self) -> T
+    where
+        T: Copy,
+    {
+        todo!()
+    }
+}
